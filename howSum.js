87dbf5c37b990172ef7c9f2f63c449e8917378a6
iarmankhan <work.armankhan@gmail.com>
2021-06-26 18:30:20 +0530

    :sparkles: memoized howSum

--- howSum.js
+++ howSum.js
@@ -7,22 +7,36 @@
 // for eg. targetSum = 7 arr = [5, 3, 4, 7] => [4, 3] or [7]
 // targetSum = 7 arr = [2, 4] => null
 
-const howSum = (targetSum, numbers) => {
+const howSum = (targetSum, numbers, memo = {}) => {
+    if(targetSum in memo) return memo[targetSum];
     if(targetSum === 0) return [];
     if(targetSum < 0) return null;
 
     for (let num of numbers){
         const remainder = targetSum - num;
-        const result = howSum(remainder, numbers);
+        const result = howSum(remainder, numbers, memo);
 
         if(result !== null){
-            return [...result, num];
+            memo[targetSum] = [...result, num];
+            return memo[targetSum];
         }
     }
 
+    memo[targetSum] = null
     return null;
 };
 
+// m = target sum
+// n = length of numbers
+
+// Brute force
+// time: O(n^m * m)
+// space: O(m)
+
+// Memoized
+// time: O(n * m * m) = O(n * m^2)
+// space: O(m^2) 
+
 console.log(howSum(7, [5,3,4,7])) 
 console.log(howSum(7, [2,4]))
-console.log(howSum(49, [7,14]))
+console.log(howSum(21221, [7,14]))
