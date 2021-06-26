74936c71185f2fb2a4921c2e4d2086b82122c88c
iarmankhan <work.armankhan@gmail.com>
2021-06-26 15:36:20 +0530

    :sparkles: can sum memoization

--- canSum.js
+++ canSum.js
@@ -7,21 +7,24 @@
 // for eg. targetSum = 7 arr = [5, 3, 4, 7] => true
 // targetSum = 7 arr = [2, 4] => false
 
-const canSum = (targetSum, numbers) => {
+const canSum = (targetSum, numbers, memo = {}) => {
+    if(targetSum in memo) return memo[targetSum];
     if(targetSum === 0) return true;
     if(targetSum < 0) return false;
 
     for(let num of numbers){
         const remainder = targetSum - num;
 
-        if(canSum(remainder, numbers) === true){
+        if(canSum(remainder, numbers, memo) === true){
+            memo[targetSum] = true
             return true
         }
     }
 
+    memo[targetSum] = false
     return false;
 }
 
 console.log(canSum(7, [5,3,4,7])) 
 console.log(canSum(7, [2,4]))
-console.log(canSum(300, [7,14]))
+console.log(canSum(21, [7,14]))
