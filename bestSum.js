0b8606a048d4c0588b16036093bfbb4dfc22665a
iarmankhan <work.armankhan@gmail.com>
2021-06-26 20:39:36 +0530

    :sparkles: best sum

---
+++ bestSum.js
@@ -0,0 +1,45 @@
+// given a target sum and an array of positive numbers
+// return the shortest array of numbers that can sum upto target number
+// return null if there's no sum
+
+// you can use one number from numbers array multiple times
+
+// for eg. targetSum = 7 arr = [5, 3, 4, 7] => [7]
+// targetSum = 7 arr = [2, 4] => null
+
+const bestSum = (targetSum, numbers) => {
+    if(targetSum === 0) return []
+    if(targetSum < 0) return null
+
+    let shortestCombination = null
+
+    for (let num of numbers){
+        const remainder = targetSum - num;
+        const remainderCombination = bestSum(remainder, numbers);
+
+        if(remainderCombination !== null){
+            const combination = [...remainderCombination, num];
+
+            if(shortestCombination === null || combination.length < shortestCombination.length){
+                shortestCombination = combination;
+            }
+        }
+    }
+
+    return shortestCombination;
+
+};
+
+
+// m = target sum
+// n = length of numbers
+
+// Brute force
+// time: O(n^m * m)
+// space: O(m^2)
+
+
+console.log(bestSum(7, [5,3,4,7])) 
+console.log(bestSum(7, [2,4]))
+console.log(bestSum(49, [7,14]))
+console.log(bestSum(8, [1, 1, 1, 2, 4]))
