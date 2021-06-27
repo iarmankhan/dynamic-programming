af434d1b5634c3c14fd4cf80f2d805ff1c2b7b02
iarmankhan <work.armankhan@gmail.com>
2021-06-27 17:47:34 +0530

    :sparkles: memoized count construct

--- countConstruct.js
+++ countConstruct.js
@@ -7,7 +7,8 @@
 // target = 'abcdef' wordBank = ['abc', 'ab', 'cd', 'def', 'd'] => 1
 // target = 'abbbba' wordBank = ['abb', 'cd', 'aa', 'abc'] => 0
 
-const countConstruct = (target, wordBank) => {
+const countConstruct = (target, wordBank, memo = {}) => {
+    if(target in memo) return memo[target];
     if(target === '') return 1;
 
     let total = 0
@@ -15,12 +16,12 @@ const countConstruct = (target, wordBank) => {
     for (let word of wordBank){
         if(target.indexOf(word) === 0){
             const suffix = target.slice(word.length);
-            const suffixResult = countConstruct(suffix, wordBank);
+            const suffixResult = countConstruct(suffix, wordBank, memo);
 
             total += suffixResult
         }
     }
-
+    memo[target] = total;
     return total;
 };
 
@@ -30,6 +31,10 @@ const countConstruct = (target, wordBank) => {
 
 // Bruteforce
 // time: O(n^m * m) 
+// space: O(m^2) 
+ 
+// Memoized
+// time: O(n * m^2) 
 // space: O(m^2) 
 
 console.log(countConstruct('abcdef', ['abc', 'ab', 'cd', 'def', 'd'])) // 1
